4f84d00cfdbec619a1975ee381a5cb12bddf018a
kiruba14112000 <kirubalovely2@gmail.com>
2022-04-28 11:51:02 +0530

    ajay change

--- src/org/lang/StateInfo.java
+++ src/org/lang/StateInfo.java
@@ -4,6 +4,11 @@ public class StateInfo {
 	public void southIndia() {
 		System.out.println("SouthIndia");
 	}
+
+	private void m1() {
+		System.out.println("kiruba");
+	}
+
 	public void northIndia() {
 		System.out.println("northIndia");
 	}
